d1a0d437f3a3580f3d7183643612daaf29682547
AU_ZU_FA <uig65332@contiwan.com>
2024-06-03 12:35:28 -0600

    feat: se añaden registros de prueba y realiza fetch en Devices

--- src/components/Devices/Devices.js
+++ src/components/Devices/Devices.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+fetch("/database/db.json")
+  .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Aquí puedes trabajar con los datos de tu JSON
+    })
+  .catch(error => console.error('Error al cargar el archivo JSON:', error));
+
 const Devices = () => {
   return (
-    <div>Devices</div>
+    <main>
+      
+    </main>
   )
 }
 
