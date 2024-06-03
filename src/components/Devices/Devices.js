66f7803eaa57654bca05896c57ffca260f143232
AU_ZU_FA <uig65332@contiwan.com>
2024-06-03 16:24:55 -0600

    feat: Se añade fetch para mostrar equipos, inicio de cards

--- src/components/Devices/Devices.js
+++ src/components/Devices/Devices.js
@@ -1,19 +1,33 @@
-import React from 'react'
-
-fetch("/database/db.json")
-  .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      // Aquí puedes trabajar con los datos de tu JSON
-    })
-  .catch(error => console.error('Error al cargar el archivo JSON:', error));
+import React, { useState, useEffect } from 'react';
+import "./Devices.css"
 
 const Devices = () => {
+  const [equipos, setEquipos] = useState([]);
+
+  useEffect(() => {
+    fetch("/database/db.json")
+      .then(response => response.json())
+      .then(data => {
+        setEquipos(data.equipos);
+        console.log(data);
+      })
+      .catch(error => console.error('Error al cargar el archivo JSON:', error));
+  }, []);
+
   return (
     <main>
-      
+      <h1 className='page_title'>Listado de equipos</h1>
+      <div className="card__container">
+        {equipos.map((equipo, index) => (
+          <div key={index} className="card">
+            <h2>{equipo.informacionGeneral.nombreDelEquipo}</h2>
+          </div>
+        ))}
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Devices
+export default Devices;
+
+/* https://uiverse.io/aadium/mighty-dog-12 */
