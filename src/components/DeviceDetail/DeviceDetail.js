908b1a3f7b5c26a7cd1f6e231034061f607109d4
AU_ZU_FA <uig65332@contiwan.com>
2024-06-04 12:01:08 -0600

    feat: change font

--- src/components/DeviceDetail/DeviceDetail.js
+++ src/components/DeviceDetail/DeviceDetail.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import "./DeviceDetail.css";
 
 const DeviceDetail = () => {
     const { id } = useParams();
