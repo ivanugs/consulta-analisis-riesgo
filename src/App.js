e45a1d4d3365c99bd5c33bce101fd91bd5af0fd0
ivanugs <ivansegundo702@gmail.com>
2024-06-10 21:59:05 -0600

    feat: removed footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Footer from "./components/Footer/Footer";
 import { Suspense } from 'react';
 import { AppRouter } from './Router';
 import React from 'react';
@@ -11,7 +10,6 @@ function App() {
       <Suspense>
       <Header />
       <AppRouter />
-      <Footer />
       </Suspense>
     </Router>
   );
