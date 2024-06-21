bb6f8fe1e26c2977513774ce7eec83ab16f3a007
Uriel Gonzalez <ivansegundo702@gmail.com>
2024-06-21 16:03:45 -0600

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
-import logo from "../../img/logo_continental.png";
+import logo from "../../img/logo.png";
 import './Header.css';
 
 
 function Header () {
     return (
         <header className="header__section">
-            <img className="header__logo" src={logo} alt="Logo Continental" />
+            <img className="header__logo" src={logo} alt="Logo" />
             <h1 className="header__title">Safety Scan</h1>
         </header>
     );
